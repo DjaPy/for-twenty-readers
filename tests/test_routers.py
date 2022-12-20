fd3a60337f586864dd678bc9bbe25cec1dad7a2f
sergey <djapy@yandex.ru>
2022-12-20 21:28:41 +0300

    Пофиксил тесты

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -1,3 +1,5 @@
+import datetime
+
 import aiohttp
 import pytest
 from fastapi import status
@@ -16,8 +18,10 @@ async def test_get_calendar(server, client, config):
     data = aiohttp.FormData()
     year = 'year'
     start_kathisma = 'start_kathisma'
+    start_date_kathisma = 'start_date_kathisma'
     data.add_field(year, 2022)
     data.add_field(start_kathisma, 1)
+    data.add_field(start_date_kathisma, datetime.date(2202, 1, 1))
     async with client.post(
         url=f'http://127.0.0.1:{config.port}/get-calendar',
         headers={'Content-Type': 'application/x-www-form-urlencoded'},
